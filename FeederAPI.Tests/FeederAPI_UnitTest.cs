4bf1ec65277dc3672a8ea9bc774bd9d7759e196f
Frank Chu (Visual Studio) <frankchu@outlook.com>
2017-03-15 22:39:13 -0700

    US0000 - added basic location controller

--- FeederAPI.Tests/FeederAPI_UnitTest.cs
+++ FeederAPI.Tests/FeederAPI_UnitTest.cs
@@ -4,10 +4,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FeederAPI.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class FeederAPI_UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod_01()
         {
         }
     }
